b9c1a5131237b186043926a333b57ce29d0b4246
Daniel Felleman <daniel.felleman@revature.com>
2024-01-29 15:36:44 -0500

    small change to trigger build after reboot

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const HOST = "0.0.0.0"
 
 
 app.get("/", (req, res) => {
-    res.status(200).send({message: "Hello from the EC2- maybe? - I opened up to HTTP and HTTPS..."})
+    res.status(200).send({message: "Hello from the EC2- maybe? - I rebooted them"})
 });
 
 app.listen(PORT, () => {
