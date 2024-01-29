5f32ce24d3d994ba0419d4f17789dda457e4fc79
Daniel Felleman <daniel.felleman@revature.com>
2024-01-29 10:20:37 -0500

    made small demo app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 8000
+const HOST = "0.0.0.0"
+
+
+app.get("/", (req, res) => {
+    res.status(200).send({message: "Hello from the EC2!"})
+});
+
+app.listen(PORT, () => {
+    console.log(`App has started. Listening on dev server at http://${HOST}:${PORT}`)
+});
